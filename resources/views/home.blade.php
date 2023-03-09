66527cb6e6709ad430c0a2e5bfc2525817f805db
sazid9 <sazidislam1998@gmail.com>
2023-03-09 21:58:38 +0600

    git add branch V2.0 9/3/23 21.58

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,5 @@
 @extends('layouts.backend')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                        <h1>Blank Page</h1>
-                        </div>
-                        <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Blank Page</li>
-                        </ol>
-                        </div>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<backend></backend>
 @endsection
